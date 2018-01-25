4c033cfac9daa4f17280fdb2e7711398bb945f31
zarrukhsharipov <zarrukhsharipov@gmail.com>
2018-01-25 11:24:22 +0500

    Baza Dannix

---
+++ ETrade/Models/Enums.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ETrade.Models
+{
+    public enum SortCriteria
+    {
+        Name,
+        Price
+    }
+    public enum SortOrder
+    {
+        ASC,
+        DESC
+    }
+}
---
+++ ETrade/Models/ProductsModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ETrade.Models
+{
+    public class ProductsModel
+    {
+        public string Category { get; set; }
+        public string Name { get; set; }
+        public SortCriteria Criteria { get; set; }
+        public SortOrder Order { get; set; }
+        public List<SelectListItem> Categories { get; set; }
+
+        public List<ProductViewModel> Products { get; set; }
+    }
+}
